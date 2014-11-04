91e8c90897a1b2f993b59e1e96e6d05bc01a4bf7
Ruiyuan Li <lry198010@gmail.com>
2014-11-04 18:16:54 +0800

    no sence for the work, can be ignore

--- test/qc.js
+++ test/qc.js
@@ -1,2 +1,18 @@
-console.log(data[0].type);
+//var drawQCdistribution = function(title, data){
+//   var div = document.createElement("div");
+//   div.innerHTML("Test for quality distribution");
+//   document.body.appendChild(div);
+//}
 
+//drawQCdistribution("QC distribution",data);
+
+var getTotalBasePerPosition = function(data){
+  var total = 0;
+  
+}
+
+$(document).ready(function(){
+  $("#testIt").click(function(){
+    getTotalBasePerPosition(data);
+  }); 
+})

6bcbb645b38470e1ff1b4eb44b9a1bdd5fceba63
Ruiyuan Li <lry198010@gmail.com>
2014-11-07 19:44:33 +0800

    finished the first version of QC for quality per base!

--- test/qc.js
+++ test/qc.js
@@ -29,11 +29,14 @@ var drawBox = function(appendTo, d, w, h, mt,mr,mb,ml,mil,mir){
                                               .attr("background-color","#dddddd")
    //svg.attr("width",w).attr("height",h);
    var axisH = 50;
+   var axisY = 50;
+   var axisYpad = 10;
    var maxQual = 45;
-   var innerW = w - ml - mr;
+   var innerW = w - ml - mr - axisY ;
    var innerH = h - mt - mb - axisH;
    var boxWidth = innerW/d.length;
    var perHeight = innerH / maxQual;
+   
    
    var fontsize = 30;
    
@@ -43,78 +46,73 @@ var drawBox = function(appendTo, d, w, h, mt,mr,mb,ml,mil,mir){
    var tickValues = []
    
    for(i=0;i<d.length;i++){
-     meanPloy[j++] = (i + 0.5) * boxWidth + ml;
+     meanPloy[j++] = (i + 0.5) * boxWidth + ml + axisY;
      meanPloy[j++] = (maxQual - d[i][1]) * perHeight + mt; 
      tickValues[i] = i + 0.5;
    }
-   //console.log(meanPloy.join(" "));
+   
+   var YticksValue = [];
+   for(i=0; i<=maxQual; i += 10){
+     YticksValue[i] = i; 
+   }
 
-   svg.append("line")
-      .attr("x1",ml)
-      .attr("x2",ml)
-      .attr("y1",mt)
-      .attr("y2",mt + innerH)
-      .attr("stroke","black");
+   var Xscalar = d3.scale.linear().domain([0,d.length]).range([0,innerW])
 
- var Xscalar = d3.scale.linear().domain([0,101]).range([0,innerW])
-
-  svg.append("g").attr("class","axis")
-                 .attr("transform","translate(" + ml + "," + (h-ml-axisH) + ")")
+   svg.append("g").attr("class","axis")
+                 .attr("transform","translate(" + (ml+ axisY) + "," + (h-mb-axisH) + ")")
                  .call(d3.svg.axis().scale(Xscalar).orient("bottom").tickValues(tickValues))
                  .selectAll("text")
                  .attr("x",function(d,i){return 20;})
                  .attr("y",0)
                  .attr("dy",".35em")
                  .attr("transform", "rotate(90)");
+   var Yscalar = d3.scale.linear().domain([0,maxQual]).range([innerH,0]);
+   svg.append("g").attr("class","axis")
+                 .attr("transform","translate(" + (ml + axisY - axisYpad) + "," + mt + ")")
+                 .call(d3.svg.axis().scale(Yscalar).orient("left").tickValues(YticksValue));
    
- //  svg.selectAll("line.aix").data(d).enter().append("line")
- //                           .attr("x1",function(d,i){return (i + 0.5) * boxWidth + ml;})
- //                           .attr("y1",function(d){return innerH + mt;})
- //                           .attr("x2",function(d,i){return (i + 0.5) * boxWidth + ml;})
- //                           .attr("y2",function(d){return innerH + mt + 5;})
- //                           .attr("stroke","black")
    svg.selectAll("rect").data(d).enter().append("rect")
-                        .attr("x",function(d,i){return i * boxWidth + mil + ml;})
+                        .attr("x",function(d,i){return i * boxWidth + mil + ml + axisY;})
                         .attr("y",function(d,i){return (maxQual - d[4]) * perHeight + mt;})
                         .attr("width",boxWidth - mil - mir)
                         .attr("height",function(d){return (d[4] - d[3]) * perHeight;})
                         .attr("stroke","black")
                         .attr("sroke-width",1)
                         .attr("fill","none");
-  svg.selectAll("line.Median").data(d).enter().append("line")
-                        .attr("x1",function(d,i){return i * boxWidth + mil + ml;})
+   svg.selectAll("line.Median").data(d).enter().append("line")
+                        .attr("x1",function(d,i){return i * boxWidth + mil + ml + axisY;})
                         .attr("y1",function(d){return (maxQual - d[2]) * perHeight + mt;})
-                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml;})
+                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml + axisY;})
                         .attr("y2",function(d){return (maxQual - d[2]) * perHeight + mt;})
                         .attr("stroke","green");
-  svg.selectAll("line.dw").data(d).enter().append("line")
-                        .attr("x1",function(d,i){return i * boxWidth + mil + ml;})
+   svg.selectAll("line.dw").data(d).enter().append("line")
+                        .attr("x1",function(d,i){return i * boxWidth + mil + ml + axisY;})
                         .attr("y1",function(d){return (maxQual - d[5]) * perHeight + mt;})
-                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml;})
+                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml + axisY;})
                         .attr("y2",function(d){return (maxQual - d[5]) * perHeight + mt;})
                         .attr("stroke","red");
-  svg.selectAll("line.dwcent").data(d).enter().append("line")
-                        .attr("x1",function(d,i){return (i + 0.5) * boxWidth + ml;})
+   svg.selectAll("line.dwcent").data(d).enter().append("line")
+                        .attr("x1",function(d,i){return (i + 0.5) * boxWidth + ml + axisY;})
                         .attr("y1",function(d){return (maxQual - d[5]) * perHeight + mt;})
-                        .attr("x2",function(d,i){return (i + 0.5) * boxWidth + ml;})
+                        .attr("x2",function(d,i){return (i + 0.5) * boxWidth + ml + axisY;})
                         .attr("y2",function(d){return (maxQual - d[3]) * perHeight + mt;})
                         .attr("stroke","red")
                         .attr("stroke-dasharray","5,2");
    
-  svg.selectAll("line.up").data(d).enter().append("line")
-                        .attr("x1",function(d,i){return i * boxWidth + mil + ml;})
+   svg.selectAll("line.up").data(d).enter().append("line")
+                        .attr("x1",function(d,i){return i * boxWidth + mil + ml + axisY;})
                         .attr("y1",function(d){return (maxQual - d[6]) * perHeight + mt;})
-                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml;})
+                        .attr("x2",function(d,i){return (i+1) * boxWidth - mir + ml + axisY;})
                         .attr("y2",function(d){return (maxQual - d[6]) * perHeight + mt;})
                         .attr("stroke","red");
-  svg.selectAll("line.upcent").data(d).enter().append("line")
-                        .attr("x1",function(d,i){return (i + 0.5) * boxWidth + ml;})
+   svg.selectAll("line.upcent").data(d).enter().append("line")
+                        .attr("x1",function(d,i){return (i + 0.5) * boxWidth + ml + axisY;})
                         .attr("y1",function(d){return (maxQual - d[6]) * perHeight + mt;})
-                        .attr("x2",function(d,i){return (i + 0.5) * boxWidth + ml;})
+                        .attr("x2",function(d,i){return (i + 0.5) * boxWidth + ml + axisY;})
                         .attr("y2",function(d){return (maxQual - d[4]) * perHeight + mt;})
                         .attr("stroke","red")
                         .attr("stroke-dasharray","5,2");
-  svg.selectAll("poly.mean").data(d).enter().append("polyline")
+   svg.append("polyline")
                         .attr("points",meanPloy.join(" "))
                         .attr("stroke","black")
                         .attr("fill","none");
